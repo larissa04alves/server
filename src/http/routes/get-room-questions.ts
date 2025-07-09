dad5e96d596f0cbe6d58939e1453a6356f67512d
Larissa Alves <alves04.larissa@gmail.com>
2025-07-09 20:18:31 -0300

    [ FEAT ] - Adiciona rotas e lógica para gerenciamento de perguntas e salas

---
+++ src/http/routes/get-room-questions.ts
@@ -0,0 +1,34 @@
+import { desc, eq } from "drizzle-orm";
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { schema } from "../../db/schema/index.ts";
+import { db } from "../../db/connection.ts";
+import { z } from "zod/v4";
+
+export const getRoomQuestions: FastifyPluginCallbackZod = (app) => {
+  app.get(
+    "/rooms/:roomId/questions",
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request) => {
+      const { roomId } = request.params;
+
+      const result = await db
+        .select({
+          questionId: schema.questions.id,
+          question: schema.questions.question,
+          answer: schema.questions.answer,
+          createdAt: schema.questions.createdAt,
+        })
+        .from(schema.questions)
+        .where(eq(schema.questions.roomId, roomId))
+        .orderBy(desc(schema.questions.createdAt));
+
+      return result;
+    }
+  );
+};
